c0b6b0883cee3a51959c53cf004305c5718c56b7
jazdelu <lushizhao@qq.com>
2015-03-14 15:17:43 +0800

    .

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elpizostore.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elpizostore.local_settings")
 
     from django.core.management import execute_from_command_line
 
