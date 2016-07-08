5e49581aaf4ff9806a29320a3df10c58927609eb
jazdelu <lushizhao@qq.com>
2016-07-08 20:19:21 +0800

    change stocklist

--- elpizostore/urls.py
+++ elpizostore/urls.py
@@ -26,7 +26,7 @@ urlpatterns = patterns('',
     url(r'^collection/', include('collection.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^press/', include('press.urls')),
-    url(r'^shop/', include('shop.urls')),
+    url(r'^stocklist/', include('shop.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- shop/views.py
+++ shop/views.py
@@ -3,5 +3,5 @@ from shop.models import Shop
 from django.template import RequestContext
 # Create your views here.
 def get_shops(request):
-	shops = Shop.objects.all()
+	shops = ''
 	return render_to_response("shop.html",{'shops': shops},context_instance=RequestContext(request))
